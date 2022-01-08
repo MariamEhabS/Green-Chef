bdaaddd49f9d9fed11da0938c001da1ee5eab527
Mariam Ehab Codes <64708267+MariamEhabS@users.noreply.github.com>
2022-01-08 02:28:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -62,8 +62,8 @@ app.post('/', async (req, res) => {
       }
     );
     console.log({crmResponse});
+    return res.redirect("/thankyou/thankyou.html");
     res.status(200).json(crmResponse);
-    // res.redirect("/thankyou/thankyou.html");
 
   } catch (error) {
     console.log({error});
